a11949a2610ff6cafba8747a14089da5278cbe5b
strombach <fredde.stromback@gmail.com>
2020-10-06 20:24:55 +0200

    changed print_B to print_D

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
   printf("The first four letters of the alphabet:\n");
   print_A();
-  print_A();
+  print_D();
   print_C();
   print_D();
   return (RETVALUE);
